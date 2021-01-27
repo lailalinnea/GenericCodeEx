d5fd045c38a7d8558ee8602d9288ce37a68c48b3
Felicia Stenvall <felicia.stenvall2244@gmail.com>
2021-01-27 09:23:04 +0100

    commenting my code

---
+++ GenericCodeEx/ListOfObjects.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCodeEx
+{
+    class ListOfObjects<T>
+    {
+        private T[] theList;              //den lista som alla element lagras i 
+        private int noOfElements = 10;    //i variabeln bestämms det att det ska finnas 10platser i listan
+        private int nextIndex;            //ser till att objectet läggs i rätt element
+
+        /// <summary>
+        /// konstruktorn skapar list of objects
+        /// </summary>
+        public ListOfObjects()
+        {
+            theList = new T[noOfElements];
+            nextIndex = 0;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        public void AddObject(T obj)
+        {
+            if (nextIndex >= theList.Length)
+            {
+                Console.WriteLine("Array length exceeded");
+                return;
+            }
+            theList[nextIndex] = obj;
+            nextIndex++;
+        }
+
+
+        public T GetObject(int index)
+        {
+            if (index >= theList.Length)
+            {
+                Console.WriteLine("Array index too high");
+                return default(T);
+            }
+            return theList[index];
+        }
+    }
+}
